f598911383556b70f8ceb6981e89f2632a41ddb0
cx <chengxing@163.com>
2022-06-19 17:47:45 +0800

    my own tags

---
+++ article/templatetags/article_tags.py
@@ -0,0 +1,17 @@
+from django import template
+from article.models import ArticlePost
+
+register = template.Library()
+
+@register.simple_tag
+def total_articles():
+    return ArticlePost.objects.count()
+
+@register.simple_tag
+def author_total_articles(user):
+    return user.article.count()
+
+@register.inclusion_tag('article/list/latest_articles.html')
+def latest_articles(n=5):
+    latest_articles = ArticlePost.objects.order_by("-created")[:n]
+    return {"latest_articles": latest_articles}
